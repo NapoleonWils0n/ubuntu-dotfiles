646e4096b8863f399d50a3849b39f4c4f9cc3bee
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-01-17 01:27:05 +0000

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,14 +1,3 @@
-;; emacs start up time ----------------------------------------------------------------------
-
-(defun efs/display-startup-time ()
-  (message "Emacs loaded in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                     (time-subtract after-init-time before-init-time)))
-           gcs-done))
-
-(add-hook 'emacs-startup-hook #'efs/display-startup-time)
-
 ;; emacs start up --------------------------------------------------------------------------
 
 (setq inhibit-startup-message t)
