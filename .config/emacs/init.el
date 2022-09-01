ebed9329a2949060d3801646fc918346002ed27a
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-09-01 18:46:20 +0100

    emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -24,6 +24,10 @@
 (setq kept-old-versions 2)
 (setq create-lockfiles nil)
 
+;; pinentry prompt in emacs
+(defvar epa-pinentry-mode)
+(setq epa-pinentry-mode 'loopback)
+
 ;; dont backup files opened by sudo or doas
 (setq backup-enable-predicate
       (lambda (name)
