f70a1f8556235d42db68e071a9dc892e846e4bce
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-10-23 22:26:40 +0100

    emacs transparency

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -78,6 +78,9 @@
 ;; hide menu bar
 (menu-bar-mode -1)
 
+;; opacity
+(add-to-list 'default-frame-alist '(alpha-background . 90))
+
 ;; start the initial frame maximized
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
