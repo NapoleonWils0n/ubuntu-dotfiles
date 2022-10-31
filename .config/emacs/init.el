a9600b8b5646140737f06a72b002881a967db59c
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-10-31 16:10:45 +0000

    tabs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -132,6 +132,93 @@
 
 ;; emacs 28 - dictionary server
 (setq dictionary-server "dict.org")
+;; centaur-tabs ---------------------------------------------------------------------------------
+
+;; ctrl shift tab
+(define-key function-key-map [(control shift iso-lefttab)] [(control shift tab)])
+
+(require 'centaur-tabs)
+(setq centaur-tabs-set-icons t
+      centaur-tabs-set-bar 'over
+      centaur-tabs-gray-out-icons 'buffer
+      centaur-tabs-set-modified-marker t
+      centaur-tabs-modified-marker "*")
+(centaur-tabs-mode t)
+(global-set-key (kbd "C-<S-tab>")  'centaur-tabs-backward)
+(global-set-key (kbd "C-<tab>") 'centaur-tabs-forward)
+(centaur-tabs-headline-match)
+
+    (defun centaur-tabs-buffer-groups ()
+      "`centaur-tabs-buffer-groups' control buffers' group rules.
+    Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+    All buffer name start with * will group to \"Emacs\".
+    Other buffer group by `centaur-tabs-get-group-name' with project name."
+      (list
+	(cond
+	 ((or (string-equal "*" (substring (buffer-name) 0 1))
+	      (memq major-mode '(magit-process-mode
+				 magit-status-mode
+				 magit-diff-mode
+				 magit-log-mode
+				 magit-file-mode
+				 magit-blob-mode
+				 magit-blame-mode
+				 )))
+	  "Emacs")
+	 ((derived-mode-p 'prog-mode)
+	  "Editing")
+	 ((derived-mode-p 'dired-mode)
+	  "Dired")
+	 ((memq major-mode '(helpful-mode
+			     help-mode))
+	  "Help")
+	 ((memq major-mode '(org-mode
+			     org-agenda-clockreport-mode
+			     org-src-mode
+			     org-agenda-mode
+			     org-beamer-mode
+			     org-indent-mode
+			     org-bullets-mode
+			     org-cdlatex-mode
+			     org-agenda-log-mode
+			     diary-mode))
+	  "OrgMode")
+	 (t
+	  (centaur-tabs-get-group-name (current-buffer))))))
+
+(defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+     (string-prefix-p "*Messages" name)
+     (string-prefix-p "*Async-native-compile-log" name)
+     (string-prefix-p "*Completions" name)
+
+     ;; Is not magit buffer.
+     (and (string-prefix-p "magit" name)
+	  (not (file-name-extension name)))
+     )))
+
+;; Save the desktop session
+(desktop-save-mode 1)
 
 
 ;; require --------------------------------------------------------------------------------------
