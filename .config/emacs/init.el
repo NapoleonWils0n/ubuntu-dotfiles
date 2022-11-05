4ad5579ef92f9adfdfcba56a1f4564c8a4fbd41b
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-11-05 02:05:54 +0000

    hide line numbers in vterm

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -473,7 +473,12 @@
 ;; custom faces
 ;; ------------------------------------------------------------------------------------------------
 
-(custom-set-faces '(org-link ((t (:inherit link :underline nil)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-link ((t (:inherit link :underline nil)))))
 
 
 ;; ------------------------------------------------------------------------------------------------
@@ -505,14 +510,14 @@
 ;; dired all the icons colour
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 
-;; vterm mode hide line numbers and position
-;;(defun nolinum ()
-;;  (setq line-number-mode nil)
-;;  (setq mode-line-percent-position nil)
-;;)
-;;
-;;(add-hook 'vterm-mode-hook 'nolinum)
+;; vterm mode hide line numbers
+(setq mode-line-percent-position nil)
 
+(defun nolinum ()
+  (setq-local line-number-mode nil)
+)
+
+(add-hook 'vterm-mode-hook 'nolinum)
 
 
 ;; ------------------------------------------------------------------------------------------------
