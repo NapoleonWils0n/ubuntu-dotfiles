fe05bf554c0995ff814e43fcda69525af83be6cc
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-02-07 19:20:21 +0000

    wl-paste

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -928,7 +928,7 @@ targets."
 (defun wl-paste ()
   (if (and wl-copy-process (process-live-p wl-copy-process))
       nil ; should return nil if we're the current paste owner
-      (shell-command-to-string "wl-paste -n | tr -d \r")))
+      (shell-command-to-string "wl-paste")))
 (setq interprogram-cut-function 'wl-copy)
 (setq interprogram-paste-function 'wl-paste)
 
