624cb14ab4679c35b09401707c44ecd78113e669
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-11-21 20:57:07 +0000

    removed duplicate code

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1398,10 +1398,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-link ((t (:inherit link :underline nil)))))
